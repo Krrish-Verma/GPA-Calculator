ffe6e349dba92cf21ec8c3c69cf79cbf29db6178
Khosraw Azizi <khosraw.azizi@gmail.com>
2022-08-01 16:14:51 -0500

    error handling

--- src/Main.java
+++ src/Main.java
@@ -74,7 +74,20 @@ public class Main {
                     "GPA Calculator",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 
-            int grade = JOptionPane.showConfirmDialog(null, new Object[] {"Grade:", gradeField}, "Please enter your grade:", JOptionPane.OK_CANCEL_OPTION);
+            int grade = JOptionPane.showConfirmDialog(null, new Object[]{"Grade:", gradeField}, "Please enter your grade:", JOptionPane.OK_CANCEL_OPTION);
+            if (grade != JOptionPane.OK_OPTION) main(new String[]{});
+
+            boolean error = true;
+            while (error) {
+                try {
+                    Integer.parseInt(gradeField.getText());
+                    error = false;
+                } catch (NumberFormatException nfe) {
+                    grade = JOptionPane.showConfirmDialog(null, new Object[]{"Grade:", gradeField}, "Please enter your grade:", JOptionPane.OK_CANCEL_OPTION);
+                    if (grade != JOptionPane.OK_OPTION) main(new String[]{});
+                }
+            }
+
             if (grade == JOptionPane.OK_OPTION) {
                 gpa += getGPA(typeOfClass, Integer.parseInt(gradeField.getText()), false);
                 maxGPA += getGPA(typeOfClass, 100, false);
@@ -90,5 +103,12 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Your weighted GPA is " + String.format("%.2f", gpa) +
                 "\nYour unweighted GPA is " + String.format("%.2f", unweighted) +
                 "\nYour CollegeBoard GPA is " + String.format("%.2f", cbGPA));
+
+        String[] runAgain = {"Yes", "No"};
+        if (JOptionPane.showOptionDialog(null,
+                "Would you like to continue calculating: ",
+                "GPA Calculator",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, runAgain, runAgain[0]) == 0) main(new String[]{});
     }
+
 }
